d1f15d01cff707a76c4fbc9f6c4d64ce64dcf75a
Zhong Jianxin <azuwis@gmail.com>
2016-08-16 15:04:08 +0800

    Support text with line breaks

--- app.rb
+++ app.rb
@@ -6,23 +6,25 @@ text = open(ARGV[0]).read.strip
 
 # puts "\e[H\e[2J"
 slice = IO.console.winsize[1]/2
-text.chars.each_slice(slice) do |line|
-  puts line.join
-  line.each do |char|
-    while input = STDIN.getch do
-      case input
-      when "\u0003"
-        exit 0
-      when char
-        putc char
-        break
-      when "\r"
-        if char == "\n"
-          puts "\n"
+text.split.each do |line|
+  line.chars.each_slice(slice) do |breaked_line|
+    puts breaked_line.join
+    breaked_line.each do |char|
+      while input = STDIN.getch do
+        case input
+        when "\u0003"
+          exit 0
+        when char
+          putc char
           break
+        when "\r"
+          if char == "\n"
+            puts "\n"
+            break
+          end
         end
       end
     end
+    puts
   end
-  puts
 end
