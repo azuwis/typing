3098d96b968c1119aa1d65407c78968c0148c78b
Zhong Jianxin <azuwis@gmail.com>
2016-08-16 15:21:17 +0800

    Use catch/throw to break loop

--- app.rb
+++ app.rb
@@ -6,25 +6,27 @@ text = open(ARGV[0]).read.strip
 
 # puts "\e[H\e[2J"
 slice = IO.console.winsize[1]/2
-text.split.each do |line|
-  line.chars.each_slice(slice) do |breaked_line|
-    puts breaked_line.join
-    breaked_line.each do |char|
-      while input = STDIN.getch do
-        case input
-        when "\u0003"
-          exit 0
-        when char
-          putc char
-          break
-        when "\r"
-          if char == "\n"
-            puts "\n"
+catch :finish do
+  text.split.each do |line|
+    line.chars.each_slice(slice) do |breaked_line|
+      puts breaked_line.join
+      breaked_line.each do |char|
+        while input = STDIN.getch do
+          case input
+          when "\u0003"
+            throw :finish
+          when char
+            putc char
             break
+          when "\r"
+            if char == "\n"
+              puts "\n"
+              break
+            end
           end
         end
       end
+      puts
     end
-    puts
   end
 end
